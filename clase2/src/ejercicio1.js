a814559d3eacc920c7d17f38f8e21e323884be12
Diego F Ramirez <DFRamirez@teco.com.ar>
2023-07-03 14:28:14 -0300

    ejercicio terminado

--- clase2/src/ejercicio1.js
+++ clase2/src/ejercicio1.js
@@ -19,8 +19,19 @@ const productos = { ...objetos[0], ...objetos[1] };
 
 const nuevaLista = Object.keys(productos);
 
-const venta1 = Object.values(objetos[0]).reduce((acc, v) => acc + v, 0); //acumula en una variable
-const venta2 = Object.values(objetos[1]).reduce((acc, v) => acc + v, 0);
+console.log(nuevaLista);
+const a = Object.values(objetos[0]);
+const b = Object.values(objetos[1]);
+let sum = 0;
+for (let i = 0; i < a.length; i++) {
+  sum = sum + a[i];
+}
+for (let i = 0; i < b.length; i++) {
+  sum = sum + b[i];
+}
+console.log(sum);
+//const venta1 = Object.values(objetos[0]).reduce((acc, v) => acc + v, 0); //acumula en una variable
+//const venta2 = Object.values(objetos[1]).reduce((acc, v) => acc + v, 0);
 
-console.log(`la lista de productos es: ${nuevaLista}`);
-console.log("El total vendido es: " + (venta1 + venta2));
+//console.log(`la lista de productos es: ${nuevaLista}`);
+//console.log("El total vendido es: " + (venta1 + venta2));
