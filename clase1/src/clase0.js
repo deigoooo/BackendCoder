74a8b328b78aec26e6d66d2b0ebd54aaa261a09e
Diego F Ramirez <DFRamirez@teco.com.ar>
2023-07-19 16:22:53 -0300

    ejercicio terminado

--- clase1/src/clase0.js
+++ clase1/src/clase0.js
@@ -22,3 +22,26 @@ console.log(isReverseInt(234, 432))  //true
 console.log(isReverseInt(-234, -432))  //true
 console.log(isReverseInt(-234, -732))  //false 
 */
+const isReverseInt = (argumento1, argumento2) => {
+  const astr = argumento1.toString(); // para la comparacion
+  const bstr = argumento2.toString(); // para la comparacion
+  let aent = 0;
+  if (argumento1 < 0) {
+    aent = argumento1 * -1;
+  } else {
+    aent = argumento1;
+  }
+  const arev = aent.toString().split("").reverse().join(""); //para ver el numero al revez entero
+  if (astr.length == bstr.length) {
+    if (argumento2 < 0) {
+      argumento2 = argumento2 * -1;
+    }
+    if (parseInt(arev) == argumento2) {
+      return true;
+    }
+    return false;
+  } else {
+    return false;
+  }
+};
+console.log(isReverseInt(-234, -732));
