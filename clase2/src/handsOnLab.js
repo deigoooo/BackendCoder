ac03d7282452c031b60ce3072d6d94ca6510f8f9
Diego F Ramirez <DFRamirez@teco.com.ar>
2023-07-25 13:54:53 -0300

    ticketera de ejemplo

--- clase2/src/handsOnLab.js
+++ clase2/src/handsOnLab.js
@@ -1,76 +1,33 @@
 class TicketManager {
-  #precioBaseDeGanancia;
-  #id;
-  constructor(empresa) {
-    this.empresa = empresa;
-    this.evento = [];
-    this.usuarios = [
-      {
-        id: 456,
-        nombre: "Juan Carlos",
-        apellido: "Castro",
-        edad: "30",
-      },
-      {
-        id: 789,
-        nombre: "Lionel",
-        apellido: "Messi",
-        edad: "35",
-      },
-      {
-        id: 532,
-        nombre: "Diego Armando",
-        apellido: "Maradona",
-        edad: "50",
-      },
-      {
-        id: 229,
-        nombre: "Antonio",
-        apellido: "Farias",
-        edad: "38",
-      },
-    ];
-    this.#id = 0;
+  #_events;
+  constructor() {
+    this.#_events = [];
   }
-  #idGen = () => {
-    this.#id++;
-    return this.#id;
-  };
-
-  agregarEvento = (nombre, lugar, precio, capacidad, fecha) => {
-    const newId = this.#idGen();
-    const participantes = [];
-    const capacidadR = capacidad || 50;
-
-    const nuevoEvento = {
-      id: newId,
-      nombre: nombre,
-      lugar: lugar,
-      precio: precio,
-      capacidad: capacidadR,
-      participantes: participantes,
-    };
-    this.evento.push(nuevoEvento);
-    console.log(this.evento);
-    return nuevoEvento;
-  };
-  agregarUsuario = (idEvento, idUsuario) => {
-    this.evento.forEach((element) => {
-      if (element.id === idEvento) {
-        const o = this.usuarios.find((usuario) => {
-          return usuario.id === idUsuario;
-        });
-        element.participantes.push(o);
-        console.log(this.evento[0].participantes);
-      }
-    });
-  };
-
-  ponerEventoEnGira = (idEvento, nuevaLocalidad, nuevaFecha) => {};
+  get events() {
+    return this.#_events;
+  }
+  set events(event) {
+    event.id = this.#getNextID();
+    this.#_events.push(event);
+  }
+  #getNextID() {
+    if (this.#_events.length === 0) return 1;
+    return this.#_events[this.#_events.length - 1].id + 1;
+  }
 }
 
 const ticketera = new TicketManager();
 
-ticketera.agregarEvento("tini", "estadio de la plata", 50, "09/09/2023", 150);
-ticketera.agregarEvento("tini", "estadio de la plata", 50, "09/09/2023", 150);
-ticketera.agregarUsuario(1, 456);
+ticketera.events = {
+  artist: "tini",
+  city: "estadio de la plata",
+  price: 50,
+  capacity: 1500,
+};
+ticketera.events = {
+  artist: "Lionel Messi",
+  city: "Velez",
+  price: 50,
+  capacity: 1500,
+};
+console.log(ticketera.events);
